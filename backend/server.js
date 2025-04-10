b081e2bfd0d711307e079936d23e1dbbfdfcd70f
Gaurav <07gaurav04kumar03@gmail.com>
2025-04-10 07:24:21 +0530

    Cors and dotenv added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { spawn } = require("child_process");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
